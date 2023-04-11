5c7f0084b516a43ba4cdd13b7718a64e17b61c51
Ayush Saxena <ayush.saxena2019@vitstudent.ac.in>
2023-04-11 08:30:43 +0530

    second commit

--- src/index.ts
+++ src/index.ts
@@ -2,7 +2,22 @@ let sales: number = 123_456_789;
 let course: string = 'TypeScript';
 let is_published: boolean = true;
 
-// the any type
+// the any type(avoid using this datatype)
 let level;
 level = 1;
 level = 'a';
+
+function render(document: any) {
+    console.log(document);
+}
+
+// Arrays
+// JavaScript arrays look like this : let number = [1,2,'3']
+let numbers: number[] = [1,2,3];
+numbers.forEach(n => n.toExponential);
+
+// Tuples
+let user: [number, string] = [1, 'Ayush'];
+
+// Enums
+enum Size {Small, Medium, Large};
